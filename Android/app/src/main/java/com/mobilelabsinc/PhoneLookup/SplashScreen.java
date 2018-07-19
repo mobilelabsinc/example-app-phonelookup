fce5fd19b156ab412fd7fb82f85bd57548ea16eb
Steve Orlando <steve.orlando@mobilelabsinc.com>
2018-07-19 17:40:04 -0400

    Remove threading for launch screen

--- Android/app/src/main/java/com/mobilelabsinc/PhoneLookup/SplashScreen.java
+++ Android/app/src/main/java/com/mobilelabsinc/PhoneLookup/SplashScreen.java
@@ -5,42 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends Activity {
-    private boolean isStop = false;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
         // Splash screen view
         setContentView(R.layout.splash);
 
-        final SplashScreen sPlashScreen = this;   
-
-        // The thread to wait for splash screen events
-        Thread mSplashThread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    synchronized (this) {
-                        // Wait given period of time or exit on touch
-                        isStop = true;
-                        wait(1000);
-                    }
-                } catch (InterruptedException ignored) {
-                }
-
-                finish();
-
-                // Run next activity
-                if (isStop) {
-                    Intent intent = new Intent();
-                    intent.setClass(sPlashScreen, Login.class);
-                    startActivity(intent);
-                }
-            }
-        };
-
-        mSplashThread.start();        
+        Intent intent = new Intent();
+        intent.setClass(this, Login.class);
+        startActivity(intent);
     }
 }
